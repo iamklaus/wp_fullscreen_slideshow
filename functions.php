fefd38476cbc06f41ddbfd3d82b9f04397964c06
iamklaus <klaus@iamklaus.org>
2023-04-15 12:03:34 +0200

    Various Fixes and improvements

--- functions.php
+++ functions.php
@@ -1,6 +1,82 @@
 <?php
-
+require_once( get_template_directory() . '/../../../wp-includes/ID3/getid3.php' );
 add_theme_support( 'post-thumbnails' );
+
+function getMedia($post) {
+
+	// The media array
+	$media = [];
+
+	// Should we display the text?
+	$notext = false;
+	$tags = get_the_tags($post);
+	if($tags) {
+		foreach($tags as $tag):
+			if($tag->name == "notext") { $notext = true; }
+		endforeach;
+	}
+
+	// Get title and subtitle, when it should be displayed
+	if($notext == false) {
+		$title = get_the_title($post);
+		$subtitle = strip_shortcodes(wp_trim_words( get_the_content($post), 80 )); 
+	} else {
+		$title = "";
+		$subtitle = ""; 
+	}
+
+	// Look for videos in the post
+	$post_content = $post->post_content;
+	$start = strpos( $post_content, '<!-- wp:video' );
+	if ($start !== false) {
+		$start = strpos( $post_content, '<!-- wp:video' );
+		$end = strpos( $post_content, '</figure>', $start );
+		$video_block = substr( $post_content, $start, $end - $start + 8 );
+		preg_match( '/id":(\d+)/', $video_block, $matches );
+		$video_id = $matches[1];
+		$video_path = get_attached_file( $video_id );
+		$getID3 = new getID3;
+		$video_info = $getID3->analyze( $video_path );
+		$video_length = $video_info['playtime_seconds'];
+		$video_url = wp_get_attachment_url( $video_id );
+
+		array_push($media,
+			array('type' => 'video',
+				'url' => $video_url,
+				'length' => $video_length * 1000,
+				'mimetype' => $video_info['mime_type'],
+				'title' => $title,
+				'subtitle' => $subtitle,
+				'uuid' => "video_".uniqid()));
+	}
+
+	//Add the featured image to the array
+	$post_thumbnail_id = get_post_thumbnail_id($post);
+	if($post_thumbnail_id) 
+		array_push($media, 
+			array('type' => 'image',
+				'url' => wp_get_attachment_image_url($post_thumbnail_id, $size = 'full'),
+				'title' => $title,
+				'subtitle' => $subtitle,
+				'uuid' => "image_".uniqid()));
+
+	//Add all gallery images to the array
+	if($gallery = get_post_gallery( get_the_ID($post), false )) {
+		$gallery_ids = explode(",", $gallery['ids']);
+
+		foreach($gallery_ids as $image_id) {
+			array_push($media, 
+				array('type' => 'image',
+					'url' => wp_get_attachment_image_url($image_id, $size = 'full'),
+					'title' => $title,
+					'subtitle' => $subtitle,
+					'uuid' => "image_".uniqid()));
+		}
+	}
+
+	shuffle($media);
+	return $media;
+}
 
 /**
  * Retrieves the image ids from a gallery.
@@ -47,11 +123,34 @@ function create_posttype() {
             'has_archive' => true,
             'rewrite' => array('slug' => 'movies'),
             'show_in_rest' => true,
-  
         )
     );
 }
 // Hooking up our function to theme setup
 add_action( 'init', 'create_posttype' );
+
+// Add a custom section to the theme options page
+function iamklaus_fullscreenslidertheme_options_section() {
+    add_settings_section( 'iamklaus_fullscreenslidertheme_options_section', 'IAMKLAUS Fullscreen Slider Theme Options', 'iamklaus_fullscreenslidertheme_options_section_callback', 'general' );
+}
+add_action( 'admin_init', 'iamklaus_fullscreenslidertheme_options_section' );
+
+// Add a numeric field option to the custom section
+function iamklaus_fullscreenslidertheme_options_field() {
+    add_settings_field( 'iamklaus_fullscreenslidertheme_options_field', 'Slider Interval for Images in Milliseconds', 'iamklaus_fullscreenslidertheme_options_field_callback', 'general', 'iamklaus_fullscreenslidertheme_options_section' );
+    register_setting( 'general', 'iamklaus_fullscreenslidertheme_options_field', 'intval' );
+}
+add_action( 'admin_init', 'iamklaus_fullscreenslidertheme_options_field' );
+
+// Display the section description
+function iamklaus_fullscreenslidertheme_options_section_callback() {
+    echo '<p>Add custom theme options here.</p>';
+}
+
+// Display the numeric field option
+function iamklaus_fullscreenslidertheme_options_field_callback() {
+    $value = get_option( 'iamklaus_fullscreenslidertheme_options_field', 0 );
+    echo '<input type="number" min="1000" step="1" name="iamklaus_fullscreenslidertheme_options_field" value="' . $value . '" />';
+}
 
 ?>
--- index.php
+++ index.php
@@ -2,131 +2,101 @@
 <html lang="en">
 
 <head>
+
     <title><?php echo get_bloginfo( 'name' ); ?></title>
     <meta charset="utf-8">
     <meta http-equiv="refresh" content="600">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=yes">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <?php wp_head(); ?>
 
-    <style>
-
-        body {
-            font-size: 40px;
-            text-shadow: black 1px 0 10px;
-        }
-
-        h1 {
-            font-size: 90px;
-        }
-        
-        .carousel-caption {
-            text-align: center;
-            vertical-align: middle;
-        }
-
-        .carousel-item {
-            height: 100vh;
-            background: no-repeat center center scroll;
-            -webkit-background-size: cover;
-            -moz-background-size: cover;
-            -o-background-size: cover;
-            background-size: cover;
-        }
-
-        .carousel-fade .carousel-item.active,
-        .carousel-fade .carousel-item-next.carousel-item-left,
-        .carousel-fade .carousel-item-prev.carousel-item-right {
-            opacity: 1;
-        }
-
-        .carousel-fade .active.carousel-item-left,
-        .carousel-fade .active.carousel-item-right {
-            opacity: 0;
-        }
-
-        .carousel-fade .carousel-item-next,
-        .carousel-fade .carousel-item-prev,
-        .carousel-fade .carousel-item.active,
-        .carousel-fade .active.carousel-item-left,
-        .carousel-fade .active.carousel-item-prev {
-            transform: translateX(0);
-            transform: translate3d(0, 0, 0);
-        }
-    </style>
 </head>
 
 <body style="background-color: black;">
 
-<div id="veluxScreenCarousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="3000">
+<div id="veluxScreenCarousel" class="carousel slide carousel-fade">
     <div class="carousel-inner">
 
-      <?php
-      $first = true;
+        <?php
+        $first = true;
+        $trigger_timeout = false;
 
-      if ( have_posts() ) :
+        $args = array(
+            'post_type' => 'post',
+            'orderby' => 'rand');
 
-        while ( have_posts() ) : the_post();
+        $query = new WP_Query( $args );
 
+        if ( $query->have_posts() ) {
+            while ( $query->have_posts() ) {
+                
+                $query->the_post();
+                $medias = getMedia($post); 
 
-            //#########################
-            //$media = get_media_embedded_in_content(apply_filters( 'the_content', get_the_content() ), 'video');
-           // $getID3 = new getID3;
-            //$file = $getID3->analyze($filename);
-            //echo ($file['playtime_string']);
-            //print_r($media);
+                foreach($medias as $media) { 
+                    if($media['type'] == 'image') {
+                        if($first) $trigger_timeout = true; ?>
+                        <div class="carousel-item <?php if($first) { echo " active "; $first=false; }   ?>" id="<?=$media['uuid']; ?>" style="background-image: url('<?=$media['url'];?>'); height: 100vh; background-position:center;"> <?php
+                    } elseif($media['type'] == 'video') { ?>
+                        <div class="carousel-item <?php if($first) { echo " active "; $first=false; }  ?>" id="<?=$media['uuid'];?>" data-interval="<?=$media['length'];?>">
+                            <video id="<?=$media['uuid'].'video' ?>" autoplay playsinline muted style="width: 100%; height: 100%; object-fit: cover;" onended="videoEnded()">
+                                <source src="<?=$media['url'];?>" type="<?=$media['mimetype']?>">                    
+                            </video> <?php
+                    } ?>
 
-
-            //$video_meta = get_post_meta( $attachment_ID, '_wp_attachment_metadata', true );
-            //echo $video_meta['length_formatted'];
-
-
-            //$test = get_post_meta(get_the_ID(), 'video_url', true);
-            //print_r( $test );
-            //echo "XXX";
-
-
-            //#########################
-
-            $notext = false;
-            $post_thumbnail_id = get_post_thumbnail_id( $post );
-            if($post_thumbnail_id) $images = [$post_thumbnail_id];
-            else $images = [];
-
-            if($gallery = get_post_gallery( get_the_ID(), false )) {
-                $gallery_ids = explode(",", $gallery['ids']);
-                $images = array_merge($images, $gallery_ids);
+                        <div class="carousel-caption">
+                            <h1><?=$media['title']?></h1>
+                            <p><?=$media['subtitle'];?></p>
+                        </div>
+                    </div> <?php
+                }
             }
-		
-	        shuffle($images);
-            $tags = get_the_tags();
-
-            if($tags) {
-                foreach($tags as $tag):
-	                if($tag->name == "notext") { $notext = true; }
-                endforeach;
-            }
- 		
-	        foreach($images as $image):
-            ?>
-              <div class="carousel-item <?php if($first) { echo " active "; $first=false; }   ?>" style="background-image: url('<?php echo wp_get_attachment_image_url($image, $size = 'full'); ?>'); height: 100vh; background-position:center;">
-              <?php if ( $notext == false ) : ?>
-                  <div class="carousel-caption">
-                      <h1><?php the_title(); ?> </h1>
-                      <p><?php echo strip_shortcodes(wp_trim_words( get_the_content(), 80 )); ?></p>
-                  </div>
-              <?php endif; ?> 
-              </div>
-
-            <?php
-            endforeach;
-        endwhile;
-      endif; ?>
+        } 
+        wp_reset_postdata(); ?>
     </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script>
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function videoEnded() {
+    $('.carousel').carousel('next');
+    console.log('Next trigger by video end');
+}
+
+$('#veluxScreenCarousel').on('slide.bs.carousel', function () {
+    var carousel = document.querySelector('.carousel');
+    var activeItem = carousel.querySelector('.carousel-item.active');
+    var nextItem = activeItem.nextElementSibling;
+    if (!nextItem) var nextItem = carousel.querySelector('.carousel-item:first-child');
+    var nextItemId = nextItem.getAttribute('id');
+
+    if(nextItemId.startsWith('video')) {
+        var video = document.querySelector(nextItemId + "video");
+        console.log(nextItemId + "video");
+        $('#' + nextItemId + 'video').get(0).currentTime = 0;
+        $('#' + nextItemId + 'video').get(0).play();
+    } else {
+        setTimeout(nextSlide, <?=get_option( 'iamklaus_fullscreenslidertheme_options_field', 5000 ); ?> + getRandomInt(500));
+    } 
+})
+
+function nextSlide() {
+    $('.carousel').carousel('next');
+}
+
+<?php if($trigger_timeout) echo "setTimeout(nextSlide, ".get_option( 'iamklaus_fullscreenslidertheme_options_field', 5000 )." + getRandomInt(500));"; ?>
+
+</script>
+
+<?php wp_footer(); ?>
 
 </body>
 </html>
